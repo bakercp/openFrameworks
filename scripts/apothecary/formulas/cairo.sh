2dd7f8ef02f078e829c52df660edfabd5ee08f60
Christopher Baker <me@christopherbaker.net>
2015-06-08 22:51:23 -0500

    Updates for cairo vs download phase.

--- scripts/apothecary/formulas/cairo.sh
+++ scripts/apothecary/formulas/cairo.sh
@@ -8,7 +8,7 @@
 # dependencies have their own formulas in cairo/depends
 #
 # following http://www.cairographics.org/end_to_end_build_for_mac_os_x,
-# we build and install dependencies into a subfolder of cairo by setting the 
+# we build and install dependencies into a subfolder of cairo by setting the
 # prefix (install location) and use a custom copy of pkg-config which returns
 # the dependent lib cflags/ldflags for that prefix (cairo/apothecary-build)
 
@@ -29,42 +29,48 @@ GIT_TAG=$VER
 
 # download the source code and unpack it into LIB_NAME
 function download() {
-	curl -LO http://cairographics.org/releases/cairo-$VER.tar.xz
 	if [ "$TYPE" == "vs" ] ; then
-		7z e cairo-$VER.tar.xz
-		7z x cairo-$VER.tar
-		mv cairo-$VER cairo
-		rm cairo-$VER.tar.xz
-		rm cairo-$VER.tar
-	else
-		tar -xf cairo-$VER.tar.xz
-		mv cairo-$VER cairo
-		rm cairo-$VER.tar.xz
-	fi
+		# Download the xz extractor.
+		curl -LO http://tukaani.org/xz/xz-5.2.1-windows.zip
+
+		# Unzip xz and save it to the local directory.
+		unzip -j xz-5.2.1-windows.zip bin_x86-64/xz.exe -d .
+  fi
+
+	curl -LO http://cairographics.org/releases/cairo-$VER.tar.xz
+	tar -xf cairo-$VER.tar.xz
+	mv cairo-$VER cairo
+	rm cairo-$VER.tar.xz
+
 	# manually download dependencies
 	apothecaryDependencies download
+
 	if [ "$TYPE" == "vs" ] ; then
+		# Get the custom cairo visual studio solution.
 		git clone https://github.com/DomAmato/Cairo-VS
+
+		# Remove the xz extractor.
+		rm xz.exe
 	fi
 }
 
 # prepare the build environment, executed inside the lib src dir
 function prepare() {
 	if [ "$TYPE" == "vs" ] ; then
-	
+
 		apothecaryDependencies prepare
-		
+
 		apothecaryDepend build libpng
 		apothecaryDepend build freetype
-		
+
 		cd ../Cairo-VS
-		
+
 		cp -Rv ../cairo/* cairo
 		cp -Rv ../freetype/* freetype
 		cp -Rv ../libpng/* libpng
 		cp -Rv ../pixman/* pixman
 		cp -Rv ../zlib/* zlib
-		
+
 		cp -v ../libpng/projects/visualc71/Win32_LIB_Release/*.lib libs
 		cp -v ../libpng/projects/visualc71/Win32_LIB_Release/ZLib/*.lib libs
 		if [ $ARCH == 32 ] ; then
@@ -72,7 +78,7 @@ function prepare() {
 		elif [ $ARCH == 64 ] ; then
 			cp -v ../freetype/objs/vc2010/x64/*.lib libs/freetype.lib
 		fi
-		
+
 	else
 		# generate the configure script if it's not there
 		if [ ! -f configure ] ; then
@@ -104,7 +110,7 @@ function build() {
 		elif [ $ARCH == 64 ] ; then
 			vs-build "cairo.sln" Build "Release|x64"
 		fi
-		 
+
 	elif [ "$TYPE" == "osx" ] ; then
 		./configure PKG_CONFIG="$BUILD_ROOT_DIR/bin/pkg-config" \
 					PKG_CONFIG_PATH="$BUILD_ROOT_DIR/lib/pkgconfig" \
@@ -117,10 +123,10 @@ function build() {
 					--disable-full-testing \
 					--disable-dependency-tracking \
 					--disable-xlib \
-					--disable-qt 
+					--disable-qt
 		make
 		make install
-	else 
+	else
 		./configure PKG_CONFIG="$BUILD_ROOT_DIR/bin/pkg-config" \
 					PKG_CONFIG_PATH="$BUILD_ROOT_DIR/lib/pkgconfig" \
 					LDFLAGS="-arch i386 -arch x86_64" \
@@ -132,7 +138,7 @@ function build() {
 					--disable-full-testing \
 					--disable-dependency-tracking \
 					--disable-xlib \
-					--disable-qt 
+					--disable-qt
 		make
 		make install
 	fi
@@ -165,15 +171,15 @@ function copy() {
 		# copy the png symlinks
 		cp -v libpng/png*.h $1/include/
 
-		
-		
+
+
 		if [ $ARCH == 32 ] ; then
-			# make the libs path 
+			# make the libs path
 			mkdir -p $1/lib/$TYPE/Win32
 			cp -v Cairo-VS/projects/Release/cairo.lib $1/lib/$TYPE/Win32/cairo-static.lib
 			cp -v Cairo-VS/projects/Release/pixman.lib $1/lib/$TYPE/Win32/pixman-1.lib
 		elif [ $ARCH == 64 ] ; then
-			# make the libs path 
+			# make the libs path
 			mkdir -p $1/lib/$TYPE/x64
 			cp -v Cairo-VS/projects/x64/Release/cairo.lib $1/lib/$TYPE/x64/cairo-static.lib
 			cp -v Cairo-VS/projects/x64/Release/pixman.lib $1/lib/$TYPE/x64/pixman-1.lib
@@ -202,10 +208,10 @@ function copy() {
 		# copy the png symlinks
 		cp -v $BUILD_ROOT_DIR/include/png* $1/include/
 
-		# make the libs path 
+		# make the libs path
 		mkdir -p $1/lib/$TYPE
 
-	
+
 		if [ "$TYPE" == "osx" ] ; then
 			cp -v $BUILD_ROOT_DIR/lib/libcairo-script-interpreter.a $1/lib/$TYPE/cairo-script-interpreter.a
 		fi
@@ -223,7 +229,7 @@ function copy() {
 
 # executed inside the lib src dir
 function clean() {
-	
+
 	# manually clean dependencies
 	apothecaryDependencies clean
 
